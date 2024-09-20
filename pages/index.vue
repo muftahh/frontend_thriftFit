02b6efd504907e74da3aa473085f55a4ee99c4f5
muftahh <muftahnaser54321@gmail.com>
2024-09-20 18:15:03 +0700

    web - 4 slider

--- pages/index.vue
+++ pages/index.vue
@@ -1,12 +1,48 @@
 <template>
   <div class="mt-custom mb-5">
     <div class="fade-in">
-      <h1 class="text-center">CTRLSS</h1>
+      <!-- slider -->
+    <Slider />
+    <!-- end slider -->
     </div>
   </div>
 </template>
 
 <script>
+import Slider from '@/components/web/slider.vue'
 export default {
+  components: {
+    Slider
+  },
+
+  head() {
+    return {
+      title: 'ThriftFit - Ya Kali Nggak Ngetrif',
+
+      //SEO WEBSITE
+      meta: [
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: '1111111'
+        },
+        {
+          hid: 'og:site_name',
+          name: 'og:site_name',
+          content: '2222222'
+        },
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          content: '/images/logo.png'
+        },
+        {
+          hid: 'description',
+          name: 'description',
+          content: '4444444'
+        }
+      ]
+    }
+  }
 }
 </script>
--- store/web/slider.js
+++ store/web/slider.js
@@ -1,3 +1,21 @@
-export const state = () => {
-  slider: {}
+export const state = () => ({
+  sliders: []
+})
+
+export const mutations = {
+  SET_SLIDERS_DATA(state, payload) {
+    state.sliders = payload
+  },
+}
+
+export const actions = {
+  getSlidersData({commit}) {
+    return new Promise((resolve, reject) => {
+      this.$axios.get('/api/web/sliders')
+      .then((response) => {
+        commit('SET_SLIDERS_DATA', response.data.data)
+        resolve()
+      })
+    })
+  }
 }
